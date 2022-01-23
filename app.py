caf03c5e77aa4be12ed387b34111aa93feddfe60
tingweii <54320425+tingweii@users.noreply.github.com>
2022-01-23 15:14:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -220,5 +220,5 @@ def handle_message(event):
 if __name__ == "__main__":
     global reportData
     reportData = {}
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
